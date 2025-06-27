6440cd675f4e7a6a24a7edaf453d578e90997f91
pranto113015 <pranto3876@gmail.com>
2025-06-27 11:47:14 +0600

    add letter grade calculation program

---
+++ letter-grade-calculation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace letter_grade_calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter student's score (0-100): ");
+        int score = Convert.ToInt32(Console.ReadLine());
+
+        if (score >= 90 && score <= 100)
+            Console.WriteLine("Grade: A");
+        else if (score >= 80)
+            Console.WriteLine("Grade: B");
+        else if (score >= 70)
+            Console.WriteLine("Grade: C");
+        else if (score >= 60)
+            Console.WriteLine("Grade: D");
+        else
+            Console.WriteLine("Grade: F");
+        }
+    }
+}
