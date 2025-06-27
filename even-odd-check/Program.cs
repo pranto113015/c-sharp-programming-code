884ac7dc28c3deb529819ca6fcf65c8def3a272f
pranto113015 <pranto3876@gmail.com>
2025-06-27 12:09:00 +0600

    add even-odd check program and project configuration

---
+++ even-odd-check/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace even_odd_check
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter an integer: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+                Console.WriteLine($"{number} is even.");
+            else
+                Console.WriteLine($"{number} is odd.");
+        }
+    }
+}
